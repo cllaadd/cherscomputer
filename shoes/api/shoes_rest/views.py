31b3765b3746e9361f3ab273fb39ffb1438aacc2
Claudia Harrison <claudia3695@gmail.com>
2022-12-02 14:14:33 -0600

    added shoe model

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Hello Alex
+from common.json import ModelEncoder
+from .models import Shoe, BinVO
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["id","manufacturer","color","model_name"]
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "color",
+        "model_name",
+        "picture_url",
+    ]
+
+
+@require_http_methods(["GET","POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoes = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeListEncoder,
+        )
+    else:
+        return JsonResponse(
+                {"message": "Did not work"},
+                status=400,
+            )
