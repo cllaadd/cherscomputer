8e87ac940903bb00f1ab7046b6e4ddb00ce61c14
Alex Reiman <reiman.alex@gmail.com>
2022-12-02 13:16:22 -0700

    Hat models, hat views beginning, register wardrobe models

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class LocationVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    section_number = models.PositiveSmallIntegerField()
+    shelf_number = models.PositiveSmallIntegerField()
+
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    picture_url = models.URLField(max_length=1000, null=True )
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name = "hats",
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return f"{self.color} {self.fabric} {self.style}"
+
+    class Meta:
+        ordering = ("style", "color", "fabric")
--- wardrobe/api/wardrobe_api/admin.py
+++ wardrobe/api/wardrobe_api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Location, Bin
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Location)
+class BinAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
