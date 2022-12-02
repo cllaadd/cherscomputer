cfcc8597ba1ae69d8f72dbcba458f473a2e3f119
Alex Reiman <reiman.alex@gmail.com>
2022-12-02 15:24:08 -0700

    regitered models, created URLs

---
+++ hats/api/hats_rest/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import api_hat, api_hats
+
+urlpatterns = [
+    path(
+        "locations/<int:location_vo_id>/hats/",
+        api_hats,
+        name="api_hats",
+        ),
+    path("hats/<int:pk>/", api_hat, name="api_hat"),
+]
