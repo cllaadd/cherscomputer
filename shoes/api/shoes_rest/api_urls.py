815398d364215b7bce1a1968ac3cbad41a93f91c
Claudia Harrison <claudia3695@gmail.com>
2022-12-02 20:19:31 -0600

    fixed urls

--- shoes/api/shoes_rest/api_urls.py
+++ shoes/api/shoes_rest/api_urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import api_shoe, api_shoes
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shoes/', api_shoes, name="api_shoes"),
+    path('bins/<int:bin_vo_id>/shoes/', api_shoes, name="api_shoes"),
     path('shoes/<int:id>/', api_shoe, name="api_shoe"),
 ]
